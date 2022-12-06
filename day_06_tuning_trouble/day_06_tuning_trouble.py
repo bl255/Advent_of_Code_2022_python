6d9e5a8a265ace9cfc1b484eb67360e1161fadf3
MV <email@email.email>
2022-12-06 09:57:34 +0100

    Small change for the function in day 6 to meet PEP 8 requirements

--- day_06_tuning_trouble/day_06_tuning_trouble.py
+++ day_06_tuning_trouble/day_06_tuning_trouble.py
@@ -1,5 +1,5 @@
-input_1 = "input_06.txt"
-# input_1 = "test_input.txt"
+# input_1 = "input_06.txt"
+input_1 = "test_input.txt"
 
 with open(input_1, mode="r") as text_file:
     whole_signal = text_file.read()
@@ -9,6 +9,7 @@ def first_unique_sequence(num_unq, signal=whole_signal):
     for num in range(len(signal) - (num_unq + 1)):
         if len(set(whole_signal[num: num + num_unq])) == num_unq:
             return num + num_unq
+    return None
 
 
 print(first_unique_sequence(4))  # for my input 1757
