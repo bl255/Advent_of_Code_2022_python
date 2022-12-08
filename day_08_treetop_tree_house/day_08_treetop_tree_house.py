a9f04bb193f423ffe9330ac5058885084f57ac32
MV <email@email.email>
2022-12-08 22:37:05 +0100

    Day 8 considering only visible trees for task 2.

--- day_08_treetop_tree_house/day_08_treetop_tree_house.py
+++ day_08_treetop_tree_house/day_08_treetop_tree_house.py
@@ -14,19 +14,18 @@ def seen_trees(num, array):
 
 
 visible_trees = np.zeros(forest.shape, dtype=int) + 1  # at the beginning filled with 1, border trees are visible
-sum_seen_trees = np.zeros(forest.shape, dtype=int)
+sum_seen_from_visible = np.zeros(forest.shape, dtype=int)
 
 # the following solution would not work if border points are considered, because of empty arrays
-
 for x in range(1, forest.shape[0] - 1):
     for y in range(1, forest.shape[0] - 1):
         u, l, d, r = forest[:x, y][::-1], forest[x, :y][::-1], forest[x + 1:, y], forest[x, y + 1:]
         visible_trees[x, y] = any([forest[x, y] > max(u), forest[x, y] > max(l),
-                                  forest[x, y] > max(d), forest[x, y] > max(r)])
-
-        sum_seen_trees[x, y] = seen_trees(forest[x, y], u) * seen_trees(forest[x, y], l) *\
-            seen_trees(forest[x, y], d) * seen_trees(forest[x, y], r)
+                                   forest[x, y] > max(d), forest[x, y] > max(r)])
+        if visible_trees[x, y]:
+            sum_seen_from_visible[x, y] = seen_trees(forest[x, y], u) * seen_trees(forest[x, y], l) *\
+                seen_trees(forest[x, y], d) * seen_trees(forest[x, y], r)
 
 
 print(visible_trees.sum())  # task 1, for my input 1851
-print(np.amax(sum_seen_trees))  # task 2, for my input 574080
+print(np.amax(sum_seen_from_visible))  # task 2, for my input 574080
